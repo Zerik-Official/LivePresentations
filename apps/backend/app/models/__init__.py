79f5b683998ab1685bc19294b74f2bcf9e4d98a5
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-09-16 00:56:20 -0500

    feat(models): export room model

--- apps/backend/app/models/__init__.py
+++ apps/backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.presentation import Presentation
+from app.models.room import Room
 from app.models.user import User
 
-__all__ = ["Presentation", "User"]
+__all__ = ["Presentation", "Room", "User"]
